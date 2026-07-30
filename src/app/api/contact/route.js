79bb3ed499f6ed071e5b8d170b9934a2e3d8c235
abdul kalam <abdulkalam.ak707@gmail.com>
2026-07-30 13:31:41 +0530

    contact us mail completed

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -14,14 +14,14 @@ export async function POST(req) {
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        user: process.env.EMAIL_USER||'enquiry.samtechsa@gmail.com',
-        pass: process.env.EMAIL_PASS || 'bnbhzsuvlwskggmw',
+        user: process.env.EMAIL_USER||'enquiry.samtechsa26@gmail.com',
+        pass: process.env.EMAIL_PASS || 'dusghgjwdzdxvmgm',
       },
     });
 
     const mailOptions = {
-      from: `"Sam Tech Contact Form" <${process.env.EMAIL_USER||'enquiry.samtechsa@gmail.com'}>`,
-      to: process.env.EMAIL_TO||'enquiry.samtechsa@gmail.com',
+      from: `"Sam Tech Contact Form" <${process.env.EMAIL_USER||'enquiry.samtechsa26@gmail.com'}>`,
+      to: process.env.EMAIL_TO||'enquiry.samtechsa26@gmail.com',
       cc: ['gm@samtechsa.com', 'operations@samtechsa.com'].join(','),
       subject: `New Contact Form Submission from ${firstName} ${lastName}`,
       html: `
@@ -32,8 +32,6 @@ export async function POST(req) {
           <tr><td style="padding: 8px; border: 1px solid #ddd;"><strong>Company</strong></td><td style="padding: 8px; border: 1px solid #ddd;">${companyName}</td></tr>
           <tr><td style="padding: 8px; border: 1px solid #ddd;"><strong>Email</strong></td><td style="padding: 8px; border: 1px solid #ddd;">${email}</td></tr>
           <tr><td style="padding: 8px; border: 1px solid #ddd;"><strong>Phone</strong></td><td style="padding: 8px; border: 1px solid #ddd;">${phone}</td></tr>
-          <tr><td style="padding: 8px; border: 1px solid #ddd;"><strong>Country</strong></td><td style="padding: 8px; border: 1px solid #ddd;">${country}</td></tr>
-          <tr><td style="padding: 8px; border: 1px solid #ddd;"><strong>Category</strong></td><td style="padding: 8px; border: 1px solid #ddd;">${category}</td></tr>
           <tr><td style="padding: 8px; border: 1px solid #ddd;"><strong>Website URL</strong></td><td style="padding: 8px; border: 1px solid #ddd;">${websiteUrl}</td></tr>
           <tr><td style="padding: 8px; border: 1px solid #ddd;"><strong>How did you find us</strong></td><td style="padding: 8px; border: 1px solid #ddd;">${howDidYouFindUs}</td></tr>
           <tr><td style="padding: 8px; border: 1px solid #ddd;"><strong>Subject</strong></td><td style="padding: 8px; border: 1px solid #ddd;">${subject}</td></tr>
@@ -42,7 +40,8 @@ export async function POST(req) {
       `,
     };
         //   <tr><td style="padding: 8px; border: 1px solid #ddd;"><strong>Job Title</strong></td><td style="padding: 8px; border: 1px solid #ddd;">${jobTitle}</td></tr>
-
+          // <tr><td style="padding: 8px; border: 1px solid #ddd;"><strong>Country</strong></td><td style="padding: 8px; border: 1px solid #ddd;">${country}</td></tr>
+          // <tr><td style="padding: 8px; border: 1px solid #ddd;"><strong>Category</strong></td><td style="padding: 8px; border: 1px solid #ddd;">${category}</td></tr>
     await transporter.sendMail(mailOptions);
 
     return Response.json({ message: 'Email sent successfully!' }, { status: 200 });
